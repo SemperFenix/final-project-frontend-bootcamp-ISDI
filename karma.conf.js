44192aced706d6879945c3241ab612656b8a8b4d
SemperFenix <dureivan@gmail.com>
2023-03-15 12:29:56 +0100

    Update exclusions paths

--- karma.conf.js
+++ karma.conf.js
@@ -28,7 +28,7 @@ module.exports = function (config) {
       // source files, that you wanna generate coverage for
       // do not include tests or libraries
       // (these files will be instrumented by Istanbul)
-      "src/**/*.ts": ["coverage"],
+      "src/**/!(*spec|*mock).ts": ["coverage"],
     },
     coverageReporter: {
       dir: require("path").join(__dirname, "coverage/"),
