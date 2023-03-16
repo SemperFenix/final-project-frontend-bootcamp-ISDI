b5eade6be6b423ce395bfde512e0c7b45331181c
SemperFenix <dureivan@gmail.com>
2023-03-16 19:38:42 +0100

    Add register test

--- src/app/register/register.component.spec.ts
+++ src/app/register/register.component.spec.ts
@@ -1,14 +1,19 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterTestingModule } from '@angular/router/testing';
-
+import { of } from 'rxjs';
+import {
+  mockAikidoUser,
+  mockAikidoUsersService,
+} from 'src/app/mocks/test.mocks';
+import { AikidoUsersService } from '../services/aikido.users.service';
 import { RegisterComponent } from './register.component';
 
 describe('RegisterComponent', () => {
   let component: RegisterComponent;
   let fixture: ComponentFixture<RegisterComponent>;
-
+  let service: AikidoUsersService;
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [RegisterComponent],
@@ -16,15 +21,42 @@ describe('RegisterComponent', () => {
         RouterTestingModule,
         HttpClientTestingModule,
         ReactiveFormsModule,
+        FormsModule,
+      ],
+      providers: [
+        {
+          provide: AikidoUsersService,
+          useValue: mockAikidoUsersService,
+        },
       ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(RegisterComponent);
     component = fixture.componentInstance;
+    service = TestBed.inject(AikidoUsersService);
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+  describe('Given the handleSubmit', () => {
+    describe('When called', () => {
+      it('Then it should call service.register', () => {
+        component.newRegisterForm.value['name'] = 'TestName';
+        component.newRegisterForm.value['lastName'] = 'TestLast';
+        component.newRegisterForm.value['age'] = 'TestAge';
+        component.newRegisterForm.value['timePracticing'] = 'TestTime';
+        component.newRegisterForm.value['email'] = 'TestMail';
+        component.newRegisterForm.value['password'] = 'TestPass';
+
+        const spyRegister = spyOn(service, 'register').and.returnValue(
+          of(mockAikidoUser)
+        );
+
+        component.handleSubmit();
+        expect(spyRegister).toHaveBeenCalled();
+      });
+    });
   });
 });
