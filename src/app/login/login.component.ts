6a1f78f0f123783f61f536299a356d4784211a30
SemperFenix <dureivan@gmail.com>
2023-03-16 15:05:48 +0100

    Add login functionality

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,17 +1,25 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Subscription } from 'rxjs';
 import { Login } from 'src/types/login';
+import { AikidoUsersService } from '../services/aikido.users.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.sass'],
 })
-export class LoginComponent {
+export class LoginComponent implements OnDestroy {
   newLoginForm: FormGroup;
-  @Output() itAdd: EventEmitter<Login>;
-  constructor(public formBuilder: FormBuilder) {
-    this.itAdd = new EventEmitter();
+  token: string;
+  token$: Subscription;
+  constructor(
+    public aikidoUsersService: AikidoUsersService,
+    public formBuilder: FormBuilder
+  ) {
+    this.token = '';
+    this.token$ = new Subscription();
+
     this.newLoginForm = formBuilder.group({
       name: ['Nombre', [Validators.required]],
       lastName: ['Apellidos', [Validators.required]],
@@ -23,11 +31,18 @@ export class LoginComponent {
   }
 
   handleSubmit() {
-    const newAikidoUser: Login = {
+    const loginUser: Login = {
       email: this.newLoginForm.value.email,
       password: this.newLoginForm.value.password,
     };
-    this.itAdd.next(newAikidoUser);
-    this.newLoginForm.reset();
+
+    this.token$ = this.aikidoUsersService.login(loginUser).subscribe((data) => {
+      this.aikidoUsersService.token$.next(data.results[0].token);
+      this.token = data.results[0].token;
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.token$.unsubscribe();
   }
 }
