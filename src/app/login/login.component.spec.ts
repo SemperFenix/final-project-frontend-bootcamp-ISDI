bbee538efd1da6b9c782b863bd63a23bf65a88db
SemperFenix <dureivan@gmail.com>
2023-03-16 19:38:36 +0100

    Add login test

--- src/app/login/login.component.spec.ts
+++ src/app/login/login.component.spec.ts
@@ -4,12 +4,22 @@ import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { AikidoUsersService } from '../services/aikido.users.service';
 
 import { LoginComponent } from './login.component';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { of } from 'rxjs';
+import { mockAikidoUsersService } from 'src/app/mocks/test.mocks';
+
+const serverResp = {
+  results: [
+    {
+      token: '',
+    },
+  ],
+};
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
-
+  let service: AikidoUsersService;
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [LoginComponent],
@@ -17,16 +27,39 @@ describe('LoginComponent', () => {
         RouterTestingModule,
         HttpClientTestingModule,
         ReactiveFormsModule,
+        FormsModule,
       ],
-      providers: [AikidoUsersService],
+      providers: [
+        {
+          provide: AikidoUsersService,
+          useValue: mockAikidoUsersService,
+        },
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(LoginComponent);
     component = fixture.componentInstance;
+    service = TestBed.inject(AikidoUsersService);
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+  describe('Given the handleSubmit method', () => {
+    describe('When called', () => {
+      it('Should call the aikidoUsers login service and next', () => {
+        component.newLoginForm.value['email'] = 'TestMail';
+        component.newLoginForm.value['password'] = 'TestPass';
+
+        const spyLogin = spyOn(service, 'login').and.returnValue(
+          of(serverResp)
+        );
+
+        component.handleSubmit();
+
+        expect(spyLogin).toHaveBeenCalled();
+      });
+    });
   });
 });
