40d5eecb0f5eeb1d2c6b8d5dd8733d9fc05cb5ad
SemperFenix <dureivan@gmail.com>
2023-03-17 17:31:47 +0100

    Update register-modal test

--- src/app/utils/modals/register-modal/register-modal.component.spec.ts
+++ src/app/utils/modals/register-modal/register-modal.component.spec.ts
@@ -1,16 +1,22 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Router } from '@angular/router';
 
 import { RegisterModalComponent } from './register-modal.component';
 
 describe('RegisterModalComponent', () => {
   let component: RegisterModalComponent;
   let fixture: ComponentFixture<RegisterModalComponent>;
-
+  const mockRouter = jasmine.createSpyObj('Router', ['navigateByUrl']);
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ RegisterModalComponent ]
-    })
-    .compileComponents();
+      declarations: [RegisterModalComponent],
+      providers: [
+        {
+          provide: Router,
+          useValue: mockRouter,
+        },
+      ],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(RegisterModalComponent);
     component = fixture.componentInstance;
@@ -19,5 +25,16 @@ describe('RegisterModalComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('When call the close modal method', () => {
+    it('call the registerModal service', () => {
+      const spy = spyOn(
+        component.modalService,
+        'registerModal'
+      ).and.callThrough();
+      component.closeModal();
+      expect(spy).toHaveBeenCalledWith(false);
+    });
   });
 });
