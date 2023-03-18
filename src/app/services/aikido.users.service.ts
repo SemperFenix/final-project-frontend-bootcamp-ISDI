e0465a7b78648a4319fbcb18d52f1cd9faad5aa7
SemperFenix <dureivan@gmail.com>
2023-03-18 22:26:14 +0100

    Add users subjects to access

--- src/app/services/aikido.users.service.ts
+++ src/app/services/aikido.users.service.ts
@@ -1,7 +1,12 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { AikidoUser, ProtoAikidoUser, UsersList } from 'src/types/aikido.user';
-import { BehaviorSubject, Observable } from 'rxjs';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import {
+  AikidoUser,
+  ProtoAikidoUser,
+  UsersList,
+  UsersServerResponse,
+} from 'src/types/aikido.user';
+import { BehaviorSubject, Observable, Subject } from 'rxjs';
 import { Login } from 'src/types/login';
 import { ServerLoginResponse } from 'src/types/server.responses';
 
@@ -13,8 +18,16 @@ export class AikidoUsersService {
   apiBaseUrl: string;
   token: string | null;
   token$: BehaviorSubject<string>;
+  private senseis: UsersList;
+  private senseis$: Subject<UsersList>;
+  private students: UsersList;
+  private students$: Subject<UsersList>;
 
   constructor(public http: HttpClient) {
+    this.senseis = { users: [], number: 0 };
+    this.senseis$ = new Subject<UsersList>();
+    this.students = { users: [], number: 0 };
+    this.students$ = new Subject<UsersList>();
     this.token = '';
     this.apiBaseUrl = 'http://localhost:4500/aikido-users';
     this.fetching = new BehaviorSubject<boolean>(false);
@@ -35,13 +48,36 @@ export class AikidoUsersService {
     ) as Observable<ServerLoginResponse>;
   }
 
-  getUsers(): Observable<UsersList> {
+  getSenseiUsers(pPage: string): Observable<UsersServerResponse> {
+    // eslint-disable-next-line no-debugger
+    // debugger;
     this.token$.value
       ? (this.token = this.token$.value)
       : (this.token = localStorage.getItem('Token'));
-    return this.http.get(this.apiBaseUrl + '/users', {
-      headers: { ['Authorization']: 'Bearer ' + this.token$ },
+    return this.http.get(this.apiBaseUrl + '/users/senseis', {
+      headers: { ['Authorization']: `Bearer ${this.token}` },
+      params: new HttpParams().set('page', pPage),
       responseType: 'json',
-    }) as Observable<UsersList>;
+    }) as Observable<UsersServerResponse>;
+  }
+  senseisUsers(pSenseis: UsersList): void {
+    this.senseis = pSenseis;
+    this.senseis$.next(this.senseis);
+  }
+
+  getStudentUsers(pPage: string): Observable<UsersServerResponse> {
+    this.token$.value
+      ? (this.token = this.token$.value)
+      : (this.token = localStorage.getItem('Token'));
+    return this.http.get(this.apiBaseUrl + '/users/students', {
+      headers: { ['Authorization']: `Bearer ${this.token}` },
+      params: new HttpParams().set('page', pPage),
+      responseType: 'json',
+    }) as Observable<UsersServerResponse>;
+  }
+
+  studentsUsers(pStudents: UsersList): void {
+    this.students = pStudents;
+    this.students$.next(this.students);
   }
 }
