0af352ff17d72f5d7263168c8eb45bd86cbfb9ea
SemperFenix <dureivan@gmail.com>
2023-03-18 13:26:50 +0100

    Add environment settings for secrets

---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  secret: 'No se lo digas a nadie',
+
+  production: true,
+};
---
+++ src/environments/set-env.js
@@ -0,0 +1,19 @@
+import { writeFile } from "fs/promises";
+import { config } from "dotenv";
+import { env } from "process";
+
+const setEnv = () => {
+  // Configure Angular `environment.ts` file path
+  const targetPath = "./src/environments/environment.ts";
+  // Load node modules
+  config({ path: "src/environments/.env" });
+  // `environment.ts` file structure
+  const envConfigFile = `export const environment = {
+  secret: '${env?.["SECRET"]}',
+  production: true,
+};
+`;
+  writeFile(targetPath, envConfigFile);
+};
+
+setEnv();
