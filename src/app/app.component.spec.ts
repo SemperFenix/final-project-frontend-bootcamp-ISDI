68e5ede74e6371664a8925a403761411bd63ae0b
SemperFenix <dureivan@gmail.com>
2023-03-19 13:21:42 +0100

    Update app title and test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -26,10 +26,10 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'frontend'`, () => {
+  it(`should have as title 'Aikido Journey'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('frontend');
+    expect(app.title).toEqual('Aikido Journey');
   });
 
   it('should render title', () => {
@@ -37,7 +37,7 @@ describe('AppComponent', () => {
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
     expect(compiled.querySelector('h1')?.textContent).toContain(
-      'Aikido Journey'
+      'AIKIDO JOURNEY'
     );
   });
 });
