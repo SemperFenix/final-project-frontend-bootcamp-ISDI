2e24741d4c8b74e5c8bc92fcf7a03ccd764ef160
SemperFenix <dureivan@gmail.com>
2023-03-19 13:21:33 +0100

    Update app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,11 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import {
-  FontAwesomeModule,
-  FaIconLibrary,
-} from '@fortawesome/angular-fontawesome';
-import {
-  faBars,
-  faChevronLeft,
-  faChevronRight,
-} from '@fortawesome/free-solid-svg-icons';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
+import { FontawesomeIconsModule } from './fontawesome/fontawesome.icons.module';
 
 @NgModule({
   declarations: [AppComponent],
@@ -22,13 +14,10 @@ import { CoreModule } from './core/core.module';
     BrowserModule,
     AppRoutingModule,
     CoreModule,
-    FontAwesomeModule,
+
+    FontawesomeIconsModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
 })
-export class AppModule {
-  constructor(library: FaIconLibrary) {
-    library.addIcons(faChevronRight, faChevronLeft, faBars);
-  }
-}
+export class AppModule {}
