47a3945ce62d6e7b8f389c9e532b5184cc143407
SemperFenix <dureivan@gmail.com>
2023-03-19 01:06:31 +0100

    Update css and elements distribution

--- src/app/core/menu/menu.component.ts
+++ src/app/core/menu/menu.component.ts
@@ -15,9 +15,7 @@ export class MenuComponent implements OnInit {
   itemsAdmin: MenuItems[];
   loggedUser: LoggedUser;
   token: string | null;
-  burger: boolean;
   constructor(private loginService: LoginService) {
-    this.burger = true;
     this.token = '';
     this.loggedUser = { email: '', id: '', role: 'logout' };
     this.items = [
@@ -90,10 +88,6 @@ export class MenuComponent implements OnInit {
     const userInfo = jose.decodeJwt(this.token) as unknown as LoggedUser;
 
     this.loginService.loggedUser(userInfo);
-  }
-
-  burgerToggle(): void {
-    this.burger = !this.burger;
   }
 
   handleLogout(): void {
