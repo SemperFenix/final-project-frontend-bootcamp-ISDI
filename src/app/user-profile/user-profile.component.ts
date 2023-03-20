ce7eacafe50372398f0727088fc165d6ef9ad12e
SemperFenix <dureivan@gmail.com>
2023-03-20 15:59:59 +0100

    Add user profile component

--- src/app/user-profile/user-profile.component.ts
+++ src/app/user-profile/user-profile.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { AikidoUser } from 'src/types/aikido.user';
+import { AikidoUsersService } from '../services/aikido.users.service';
+import { LoginService } from '../services/login.service';
 
 @Component({
   selector: 'app-user-profile',
   templateUrl: './user-profile.component.html',
-  styleUrls: ['./user-profile.component.scss']
+  styleUrls: ['./user-profile.component.scss'],
 })
 export class UserProfileComponent {
+  currentUser$: Observable<AikidoUser>;
 
+  constructor(
+    private aikidoUsersService: AikidoUsersService,
+    private loginService: LoginService
+  ) {
+    this.currentUser$ = this.aikidoUsersService.getCurrentUser(
+      this.loginService.userLogged$.value.id
+    );
+  }
 }
