09a6aabc757677ddf66d42d0cafb0c0e3c274c2c
SemperFenix <dureivan@gmail.com>
2023-03-23 14:56:21 +0100

    Add firebase functionality in register

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { first, Subscription } from 'rxjs';
 import { ProtoAikidoUser } from 'src/types/aikido.user';
 
 import { AikidoUsersService } from '../services/aikido.users.service';
 import { ModalHandlerService } from '../services/modal-handler.service';
+
+import {
+  Storage,
+  ref,
+  uploadBytes,
+  getDownloadURL,
+} from '@angular/fire/storage';
 
 @Component({
   selector: 'app-register',
@@ -16,12 +23,14 @@ export class RegisterComponent {
   fetching: boolean;
   registerModal: boolean;
   subscription: Subscription;
+  private storage: Storage;
 
   constructor(
     private aikidoUsersService: AikidoUsersService,
     private handleModalService: ModalHandlerService,
     public formBuilder: FormBuilder
   ) {
+    this.storage = inject(Storage);
     this.subscription = this.handleModalService
       .getRegisterModal()
       .subscribe((value) => {
@@ -39,14 +48,37 @@ export class RegisterComponent {
     });
   }
 
-  handleSubmit() {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  async uploadImage(event: any) {
+    const file: File = event.target.files[0];
+    const imgRef = ref(
+      this.storage,
+      `avatars/${this.newRegisterForm.value.email}`
+    );
+
+    await uploadBytes(imgRef, file);
+  }
+
+  async handleSubmit() {
+    const avatarRef = ref(
+      this.storage,
+      `avatars/${this.newRegisterForm.value.email}`
+    );
+
+    const avatar = await getDownloadURL(avatarRef);
+
     const newAikidoUser: ProtoAikidoUser = {
       name: this.newRegisterForm.value.name,
       lastName: this.newRegisterForm.value.lastName,
-      age: this.newRegisterForm.value.age,
-      timePracticing: this.newRegisterForm.value.timePracticing,
+      age: this.newRegisterForm.value.age
+        ? this.newRegisterForm.value.age
+        : 'Es un misterio...',
+      timePracticing: this.newRegisterForm.value.timePracticing
+        ? this.newRegisterForm.value.timePracticing
+        : 'N/C',
       email: this.newRegisterForm.value.email,
       password: this.newRegisterForm.value.password,
+      avatar: avatar,
       grade: '6º kyu',
     };
     this.aikidoUsersService.register(newAikidoUser).pipe(first()).subscribe();
