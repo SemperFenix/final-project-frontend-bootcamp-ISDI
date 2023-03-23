982fce68bd6fd43c1467f362bb3bc3705412a96a
SemperFenix <dureivan@gmail.com>
2023-03-23 14:55:29 +0100

    Add firebase config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,12 @@ import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { FontawesomeIconsModule } from './fontawesome/fontawesome.icons.module';
 
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { AngularFireStorageModule } from '@angular/fire/compat/storage';
+import { environment } from '../environments/environment';
+
 @NgModule({
   declarations: [AppComponent],
   imports: [
@@ -14,8 +20,11 @@ import { FontawesomeIconsModule } from './fontawesome/fontawesome.icons.module';
     BrowserModule,
     AppRoutingModule,
     CoreModule,
-
     FontawesomeIconsModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireDatabaseModule,
+    AngularFirestoreModule,
+    AngularFireStorageModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/register/register.module.ts
+++ src/app/register/register.module.ts
@@ -5,6 +5,13 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { ModalsModule } from '../utils/modals/modals.module';
 import { RegisterRoutingModule } from './register-routing.module';
 import { RegisterModalComponent } from '../utils/modals/register-modal/register-modal.component';
+// import { AngularFireModule } from '@angular/fire/compat';
+// import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
+// import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { AngularFireStorageModule } from '@angular/fire/compat/storage';
+import { environment } from 'src/environments/environment';
+import { getStorage, provideStorage } from '@angular/fire/storage';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 
 @NgModule({
   declarations: [RegisterComponent, RegisterModalComponent],
@@ -13,6 +20,12 @@ import { RegisterModalComponent } from '../utils/modals/register-modal/register-
     ReactiveFormsModule,
     ModalsModule,
     RegisterRoutingModule,
+    // AngularFireModule.initializeApp(environment.firebase),
+    // AngularFireDatabaseModule,
+    // AngularFirestoreModule,
+    AngularFireStorageModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideStorage(() => getStorage()),
   ],
 })
 export class RegisterModule {}
