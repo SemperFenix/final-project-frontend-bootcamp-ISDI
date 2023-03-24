526ee3ed81ef0d298a112b31e525dbb82e133cba
SemperFenix <dureivan@gmail.com>
2023-03-24 13:34:09 +0100

    Refactor code for clarity

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,10 +1,10 @@
 import { Component, NgZone, OnDestroy } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Subscription } from 'rxjs';
-import { LoggedUser, Login } from 'src/types/login';
+import { Login } from 'src/types/login';
 import { AikidoUsersService } from '../services/aikido.users.service';
 import { LoginService } from '../services/login.service';
-import * as jose from 'jose';
+
 import { Router } from '@angular/router';
 
 @Component({
@@ -12,21 +12,17 @@ import { Router } from '@angular/router';
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.sass'],
 })
-export class LoginComponent implements OnDestroy {
+export class LoginComponent {
   newLoginForm: FormGroup;
-  token: string;
-  token$: Subscription;
+
   login: string;
   constructor(
-    private aikidoUsersService: AikidoUsersService,
     private loginService: LoginService,
     public formBuilder: FormBuilder,
     private router: Router,
     private zone: NgZone
   ) {
     this.login = 'logout';
-    this.token = '';
-    this.token$ = new Subscription();
 
     this.newLoginForm = formBuilder.group({
       email: ['', [Validators.required]],
@@ -40,23 +36,15 @@ export class LoginComponent implements OnDestroy {
       password: this.newLoginForm.value.password,
     };
 
-    this.token$ = this.aikidoUsersService.login(loginUser).subscribe((data) => {
+    this.loginService.login(loginUser).subscribe((data) => {
       if (!data) return;
-      this.aikidoUsersService.token$.next(data);
-      this.token = data;
-      localStorage.setItem('Token', this.token);
-      const userInfo = jose.decodeJwt(this.token) as unknown as LoggedUser;
+      this.loginService.token$.next(data);
 
       this.newLoginForm.reset();
-      this.loginService.userLogged$.next(userInfo);
 
       this.zone.run(() => {
         this.router.navigateByUrl('/my-profile');
       });
     });
-  }
-
-  ngOnDestroy(): void {
-    this.token$.unsubscribe();
   }
 }
--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -1,17 +1,42 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { BehaviorSubject, Observable } from 'rxjs';
-import { LoggedUser } from 'src/types/login';
+import { BehaviorSubject, map, Observable } from 'rxjs';
+import { LoggedUser, Login } from 'src/types/login';
+import { ServerLoginResponse } from 'src/types/server.responses';
+import * as jose from 'jose';
 
 @Injectable({
   providedIn: 'root',
 })
 export class LoginService {
+  apiBaseUrl: string;
+  token$: BehaviorSubject<string>;
+
   userLogged$: BehaviorSubject<LoggedUser>;
-  constructor() {
+  constructor(public http: HttpClient) {
+    this.apiBaseUrl = 'http://localhost:4500/aikido-users';
+    this.token$ = new BehaviorSubject<string>('');
+
     this.userLogged$ = new BehaviorSubject<LoggedUser>({
       id: '',
       email: '',
       role: 'logout',
     } as LoggedUser);
+  }
+
+  login(login: Login): Observable<string> {
+    return (
+      this.http.post(this.apiBaseUrl + '/login', {
+        user: login,
+      }) as Observable<ServerLoginResponse>
+    ).pipe(
+      map((data) => {
+        const token = data.results[0].token;
+        localStorage.setItem('Token', token);
+        const userInfo = jose.decodeJwt(token) as unknown as LoggedUser;
+        this.userLogged$.next(userInfo);
+        return token;
+      })
+    );
   }
 }
