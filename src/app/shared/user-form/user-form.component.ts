af7b9efac72b2c3b194e64b542391821d8d8deec
SemperFenix <dureivan@gmail.com>
2023-03-27 00:47:15 +0200

    Fix sonar errors

--- src/app/shared/user-form/user-form.component.ts
+++ src/app/shared/user-form/user-form.component.ts
@@ -42,13 +42,7 @@ export class UserFormComponent implements OnInit {
       email: [this.userData.email, [Validators.required]],
       password: ['', [Validators.required]],
     });
-    // if (!this.userData.password) this.toggleEdit();
   }
-
-  // private toggleEdit() {
-  //   const fieldset = document.querySelector('fieldset') as HTMLFieldSetElement;
-  //   fieldset.disabled = !fieldset.disabled;
-  // }
 
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   saveImage(event: any) {
