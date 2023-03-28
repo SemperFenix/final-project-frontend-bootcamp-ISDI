454ba44d024b9eafb5b3a3e0affd941e7eeec0a3
SemperFenix <dureivan@gmail.com>
2023-03-28 22:10:03 +0200

    Update functionality

--- src/app/shared/tech-card/tech-card.component.ts
+++ src/app/shared/tech-card/tech-card.component.ts
@@ -1,4 +1,6 @@
 import { Component, Input } from '@angular/core';
+import { Router } from '@angular/router';
+import { TechDetailsService } from 'src/app/services/techs/tech-details.service';
 import { Tech } from 'src/types/tech';
 
 @Component({
@@ -8,4 +10,14 @@ import { Tech } from 'src/types/tech';
 })
 export class TechCardComponent {
   @Input() tech!: Tech;
+
+  constructor(
+    private techDetailsService: TechDetailsService,
+    private router: Router
+  ) {}
+
+  saveActualTech(tech: Tech): void {
+    this.techDetailsService.currentTech.next(tech);
+    this.router.navigateByUrl('/tech/details');
+  }
 }
