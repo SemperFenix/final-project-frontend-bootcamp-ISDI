d2e7635966fd62e92aea45951dbd2e8bfb61699f
SemperFenix <dureivan@gmail.com>
2023-03-28 10:07:57 +0200

    Update service test

--- src/app/services/modal-handler.service.spec.ts
+++ src/app/services/modal-handler.service.spec.ts
@@ -13,4 +13,20 @@ describe('ModalHandlerService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  describe('When call the setRegisterModal method', () => {
+    it('set this.registerModal value', () => {
+      expect(service.registerModal.value).toBeFalse();
+      service.setRegisterModal(true);
+      expect(service.registerModal.value).toBeTrue();
+    });
+  });
+
+  describe('When call the setErrorModal method', () => {
+    it('set this.errorModal value', () => {
+      expect(service.errorModal.value).toBeFalse();
+      service.setErrorModal(true);
+      expect(service.errorModal.value).toBeTrue();
+    });
+  });
 });
