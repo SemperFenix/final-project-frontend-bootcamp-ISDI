251e6b81a305c1c77a29ac000d578db6ce30f603
SemperFenix <dureivan@gmail.com>
2023-03-28 17:57:48 +0200

    Add manage techs service and provisional test

--- src/app/services/aikido-users/manage-techs.service.spec.ts
+++ src/app/services/aikido-users/manage-techs.service.spec.ts
@@ -1,3 +1,4 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 
 import { ManageTechsService } from './manage-techs.service';
@@ -6,7 +7,7 @@ describe('ManageTechsService', () => {
   let service: ManageTechsService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({ imports: [HttpClientTestingModule] });
     service = TestBed.inject(ManageTechsService);
   });
 
--- src/app/services/aikido-users/manage-techs.service.ts
+++ src/app/services/aikido-users/manage-techs.service.ts
@@ -1,9 +1,63 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { map, Observable } from 'rxjs';
+import { AikidoUser } from 'src/types/aikido.user';
+import { ServerCompleteUserResponse } from 'src/types/server.responses';
+import { LoginService } from '../login.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ManageTechsService {
+  apiBaseUrl: string;
 
-  constructor() { }
+  constructor(public http: HttpClient, private loginService: LoginService) {
+    this.apiBaseUrl = 'http://localhost:4500/aikido-users';
+  }
+
+  addTechToLearn(pTechId: string): Observable<AikidoUser> {
+    return (
+      this.http.patch(
+        `${this.apiBaseUrl}/add-tech/${this.loginService.userLogged$.value.id}`,
+        {
+          userId: this.loginService.userLogged$.value.id,
+          tech: { id: pTechId },
+        },
+        {
+          headers: {
+            ['Authorization']: `Bearer ${this.loginService.token$.value}`,
+          },
+          responseType: 'json',
+        }
+      ) as Observable<ServerCompleteUserResponse>
+    ).pipe(
+      map((data) => {
+        this.loginService.currentUser$.next(data.results[0]);
+        return data.results[0];
+      })
+    );
+  }
+
+  removeTechToLearn(pTechId: string): Observable<AikidoUser> {
+    return (
+      this.http.patch(
+        `${this.apiBaseUrl}/remove-tech/${this.loginService.userLogged$.value.id}`,
+        {
+          userId: this.loginService.userLogged$.value.id,
+          tech: { id: pTechId },
+        },
+        {
+          headers: {
+            ['Authorization']: `Bearer ${this.loginService.token$.value}`,
+          },
+          responseType: 'json',
+        }
+      ) as Observable<ServerCompleteUserResponse>
+    ).pipe(
+      map((data) => {
+        this.loginService.currentUser$.next(data.results[0]);
+        return data.results[0];
+      })
+    );
+  }
 }
