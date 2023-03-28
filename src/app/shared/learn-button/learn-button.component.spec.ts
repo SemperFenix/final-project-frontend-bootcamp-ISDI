5b57b2149e183d91fe52536379f3b39d134a265b
SemperFenix <dureivan@gmail.com>
2023-03-28 17:54:33 +0200

    Update learn button

--- src/app/shared/learn-button/learn-button.component.spec.ts
+++ src/app/shared/learn-button/learn-button.component.spec.ts
@@ -1,3 +1,4 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LearnButtonComponent } from './learn-button.component';
@@ -8,9 +9,9 @@ describe('LearnButtonComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ LearnButtonComponent ]
-    })
-    .compileComponents();
+      declarations: [LearnButtonComponent],
+      imports: [HttpClientTestingModule],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(LearnButtonComponent);
     component = fixture.componentInstance;
--- src/app/shared/learn-button/learn-button.component.ts
+++ src/app/shared/learn-button/learn-button.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { first } from 'rxjs';
+import { ManageTechsService } from 'src/app/services/aikido-users/manage-techs.service';
+import { LoginService } from 'src/app/services/login.service';
+import { AikidoUser } from 'src/types/aikido.user';
+import { Tech } from 'src/types/tech';
 
 @Component({
   selector: 'app-learn-button',
   templateUrl: './learn-button.component.html',
-  styleUrls: ['./learn-button.component.scss']
+  styleUrls: ['./learn-button.component.scss'],
 })
 export class LearnButtonComponent {
+  @Input() techId!: Tech['id'];
+  @Input() user!: AikidoUser;
 
+  constructor(
+    private manageTechs: ManageTechsService,
+    private loginService: LoginService
+  ) {}
+
+  // Comentado para futura implementación
+  // ngOnInit(): void {
+  //   const button = document.querySelector('button') as HTMLButtonElement;
+
+  //   const userLearnt = this.techsLearnt.map((item) => {
+  //     item.id === this.techId;
+  //   });
+
+  //   const userInProgress = this.techsInProgress.map((item) => {
+  //     item.id === this.techId;
+  //   });
+  //   if (userLearnt.length !== 0 || userInProgress.length !== 0)
+  //     button.disabled = true;
+  // }
+
+  handleAddTech(pTechId: Tech['id']) {
+    this.loginService.getCurrentUser(this.user.id);
+    console.log(pTechId);
+    this.manageTechs.addTechToLearn(pTechId).pipe(first()).subscribe();
+  }
 }
