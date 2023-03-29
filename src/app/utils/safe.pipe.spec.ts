fa5dc22143ca4d35474b07f1b5526037c691e5a1
SemperFenix <dureivan@gmail.com>
2023-03-29 10:18:39 +0200

    Update test for pipe

--- src/app/utils/safe.pipe.spec.ts
+++ src/app/utils/safe.pipe.spec.ts
@@ -1,10 +1,33 @@
+import { TestBed } from '@angular/core/testing';
 import { DomSanitizer } from '@angular/platform-browser';
 import { SafePipe } from './safe.pipe';
 
 describe('SafePipe', () => {
+  let pipe: SafePipe;
+  let sanitizer: DomSanitizer;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [DomSanitizer],
+    });
+
+    sanitizer = TestBed.inject(DomSanitizer);
+    pipe = new SafePipe(sanitizer);
+  });
+
   it('create an instance', () => {
-    const sanitizer = DomSanitizer.arguments;
-    const pipe = new SafePipe(sanitizer);
     expect(pipe).toBeTruthy();
+  });
+
+  it('should sanitize an unsafe URL', () => {
+    const unsafeUrl = 'javascript:alert("hello")';
+    const safeUrl = pipe.transform(unsafeUrl);
+    expect(safeUrl).toBeDefined();
+  });
+
+  it('should return null for empty input', () => {
+    const emptyUrl = '';
+    const safeUrl = pipe.transform(emptyUrl);
+    expect(safeUrl).toBeNull();
   });
 });
