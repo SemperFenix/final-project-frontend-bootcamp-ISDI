92502ab882838f17a936782210d1ac5424fa98ee
SemperFenix <dureivan@gmail.com>
2023-03-29 16:46:30 +0200

    Add progress functionality

--- src/app/services/aikido-users/manage-techs.service.ts
+++ src/app/services/aikido-users/manage-techs.service.ts
@@ -3,7 +3,9 @@ import { Injectable } from '@angular/core';
 import { map, Observable } from 'rxjs';
 import { AikidoUser } from 'src/types/aikido.user';
 import { ServerCompleteUserResponse } from 'src/types/server.responses';
+import { Tech } from 'src/types/tech';
 import { LoginService } from '../login.service';
+import { UserDetailService } from './user-detail.service';
 
 @Injectable({
   providedIn: 'root',
@@ -11,7 +13,11 @@ import { LoginService } from '../login.service';
 export class ManageTechsService {
   apiBaseUrl: string;
 
-  constructor(public http: HttpClient, private loginService: LoginService) {
+  constructor(
+    public http: HttpClient,
+    private loginService: LoginService,
+    private userToDetailService: UserDetailService
+  ) {
     this.apiBaseUrl = 'http://localhost:4500/aikido-users';
   }
 
@@ -56,6 +62,33 @@ export class ManageTechsService {
     ).pipe(
       map((data) => {
         this.loginService.currentUser$.next(data.results[0]);
+        return data.results[0];
+      })
+    );
+  }
+
+  progressTech(
+    pUserId: AikidoUser['id'],
+    pTechId: Tech['id']
+  ): Observable<AikidoUser> {
+    return (
+      this.http.patch(
+        `${this.apiBaseUrl}/update/admin/${this.loginService.userLogged$.value.id}`,
+        {
+          userId: this.loginService.userLogged$.value.id,
+          user: { id: pUserId },
+          tech: { id: pTechId },
+        },
+        {
+          headers: {
+            ['Authorization']: `Bearer ${this.loginService.token$.value}`,
+          },
+          responseType: 'json',
+        }
+      ) as Observable<ServerCompleteUserResponse>
+    ).pipe(
+      map((data) => {
+        this.userToDetailService.userToDetail$.next(data.results[0]);
         return data.results[0];
       })
     );
